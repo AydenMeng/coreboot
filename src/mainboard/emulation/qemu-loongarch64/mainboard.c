56c3e8f8dcac444c7e0150622dc8b24a4d19cc18
Ayden Meng <aydenmeng@yeah.net>
2026-06-28 22:08:38 +0800

    add debug print

--- src/mainboard/emulation/qemu-loongarch64/mainboard.c
+++ src/mainboard/emulation/qemu-loongarch64/mainboard.c
@@ -2,10 +2,15 @@
 #include <bootblock_common.h>
 #include <console/console.h>
 
+#define UART_THR ((volatile uint8_t *)0x1fe001e0)
+static inline void uart_putc(char c) { *UART_THR = c; }
+
 void bootblock_mainboard_early_init(void)
 {
+	uart_putc('B');
 }
 
 void bootblock_mainboard_init(void)
 {
+	uart_putc('F');
 }
